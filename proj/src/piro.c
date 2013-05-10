f4b3ed94c099bdfbb6f52b2e1d708ab0a57d2d66
k.kanisawa <k.kanisawa@mahou.co.jp>
2013-05-10 17:19:17 +0900

    引数表示するように変更

--- proj/src/piro.c
+++ proj/src/piro.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-  printf("piro\n");
+  printf("----\n");
+  for (int i = 1; i < argc; i++) {
+	printf("%s\n", argv[i]);
+  }
+  printf("----\n");
   return 0;
 }
